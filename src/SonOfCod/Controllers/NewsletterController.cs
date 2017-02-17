0dccfbb032889bd63a1e68ee217f131d6de1baba
levibibo <levibibo@gmail.com>
2017-02-17 11:38:27 -0800

    Add newsletter routes and views.

--- src/SonOfCod/Controllers/NewsletterController.cs
+++ src/SonOfCod/Controllers/NewsletterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,12 @@ namespace SonOfCod.Controllers
     {
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Signup()
         {
             return View();
         }
