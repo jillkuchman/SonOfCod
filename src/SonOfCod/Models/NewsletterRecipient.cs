252ea98f396b356466b744f03842436253c9fdff
levibibo <levibibo@gmail.com>
2017-02-17 12:24:27 -0800

    Add NewsletterRecipient model.

---
+++ src/SonOfCod/Models/NewsletterRecipient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonOfCod.Models
+{
+    public class NewsletterRecipient
+    {
+        [Key]
+        public int Id { get; set; }
+        public string AppUserId { get; set; }
+        public virtual ApplicationUser AppUser { get; set; }
+    }
+}
